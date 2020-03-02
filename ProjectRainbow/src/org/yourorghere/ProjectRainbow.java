9fa4f315c88296cfbbfc8280c385d21544880cbb
OmkarArora <aroraomkar@gmail.com>
2020-03-02 21:56:04 +0530

    Rainbow formation commenced

--- ProjectRainbow/src/org/yourorghere/ProjectRainbow.java
+++ ProjectRainbow/src/org/yourorghere/ProjectRainbow.java
@@ -79,30 +79,43 @@ public class ProjectRainbow implements GLEventListener {
         glu.gluOrtho2D(0,640,0,480);
     }
     
-    
-//    private float x = -1.0f;
-//    private float y = 0.5f;
-    private int x = 10;
+
+    private int x = 5;
     private int y = 300;
     public void display(GLAutoDrawable drawable) {
         GL gl = drawable.getGL();
 
         // Clear the drawing area
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-//        gl.glLoadIdentity();
         drawCircle(gl);
 
         gl.glColor3f(1.0f, 1.0f, 1.0f );
         gl.glLineWidth(5.0f);
         gl.glBegin(GL.GL_LINES);
-//            gl.glVertex2f(-1.0f, 0.5f);
-//            gl.glVertex2f(x,y);
             gl.glVertex2i(0, 300);
             gl.glVertex2i(x,y);
         gl.glEnd();
         
-        if(x<=230)
-            x += 10;
+        if(x<=236)
+            x += 1;
+        else{
+            //white ray now touching the rain drop
+            
+            //Red ray
+            
+            //Orange ray
+            
+            //Yellow ray
+            
+            //Green ray
+            
+            //Blue ray
+            
+            //Indigo ray
+            
+            //Violet ray  
+            
+        }
 
        gl.glFlush();
         
@@ -113,10 +126,6 @@ public class ProjectRainbow implements GLEventListener {
         gl.glBegin(GL.GL_POINTS);
         gl.glColor3f(0.0f, 0.0f, 1.0f);
         
-//        for(int i=0;i<360;i++){
-//            double theta = Math.toRadians(i);
-//            gl.glVertex2f(0.5f*(float)Math.sin(theta),0.5f*(float)Math.cos(theta));
-//        }
             for(int i=0;i<360;i++){
             double theta = Math.toRadians(i);
             gl.glVertex2d(320+100*Math.sin(theta),240+100*Math.cos(theta));
